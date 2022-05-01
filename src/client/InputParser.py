674db8cec02f46553009c26afc953ba236fa5ccf
Youssef1313 <youssefvictor00@gmail.com>
2022-05-01 11:20:24 +0200

    More refactoring

--- src/client/InputParser.py
+++ src/client/InputParser.py
@@ -17,32 +17,41 @@ class InputParser:
             return
         raise ValueError(error)
 
+    def __is_current_string_and_advance(self, s) -> bool:
+        s_length = len(s)
+        if self.input[self.position:self.position + s_length] == s:
+            self.position += s_length
+            return True
+        return False
+
+    def __is_current_string(self, s) -> bool:
+        s_length = len(s)
+        if self.input[self.position:self.position + s_length] == s:
+            return True
+        return False
+
     def __parse_method(self) -> HttpMethod:
-        if self.input[self.position:self.position + len("GET")].upper() == "GET":
-            self.position += len("GET")
+        if self.__is_current_string_and_advance("GET"):
             return HttpMethod.GET
-        elif self.input[self.position:self.position + len("POST")].upper() == "POST":
-            self.position += len("POST")
+        elif self.__is_current_string_and_advance("POST"):
             return HttpMethod.POST
         raise ValueError(f"Expected a GET or POST method at position '{self.position}'.")
 
     def __parse_string(self) -> str:
         start_index = self.position
         while (not self.__reached_end() and
-               self.input[self.position] != ' ' and
-               self.input[self.position] != '\r'):
+               not self.__is_current_string(' ') and
+               not self.__is_current_string('\r\n')):
             self.position += 1
         return self.input[start_index:self.position]
 
-    def __try_parse_port(self) -> int:
+    def __parse_port(self) -> int:
         start_index = self.position
         while (not self.__reached_end() and
                self.input[self.position].isdigit()):
             self.position += 1
         if start_index == self.position:
-            # No digits were found. The port is optional, so we
-            # don't throw an exception. Instead, we return None.
-            return None
+            raise ValueError(f"Expected a port number at position '{self.position}'.")
 
         return int(self.input[start_index:self.position])
 
@@ -84,7 +93,7 @@ class InputParser:
                     # If there is a space, then we expect a port number.
                     # We increment position to eat the space and parse the port.
                     self.position += 1
-                    data.port_number = self.__try_parse_port()
+                    data.port_number = self.__parse_port()
                 else:
                     # If there is no space, then the port number was omitted.
                     data.port_number = None
