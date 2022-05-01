30e25a9ee9b085cfce1182df561547cd0dc4d3aa
Youssef1313 <youssefvictor00@gmail.com>
2022-05-01 10:40:54 +0200

    Add small unit tests and fix bugs

--- src/client/InputParser.py
+++ src/client/InputParser.py
@@ -1,6 +1,6 @@
-from client.HttpRequest import HttpRequest
-from client.HttpMethod import HttpMethod
-from client.ParsingState import ParsingState
+from src.client.HttpRequest import HttpRequest
+from src.client.HttpMethod import HttpMethod
+from src.client.ParsingState import ParsingState
 
 
 class InputParser:
@@ -9,19 +9,21 @@ class InputParser:
         self.position = 0
 
     def __reached_end(self) -> bool:
-        return self.position > len(self.input)
+        return self.position >= len(self.input)
 
     def __eat_required(self, tokens_to_eat: list[str], error: str) -> None:
         for token in tokens_to_eat:
-            if self.input[self.position:self.position + len(token) + 1] == token:
+            if self.input[self.position:self.position + len(token)] == token:
                 self.position += len(token)
                 return
         raise ValueError(error)
 
     def __parse_method(self) -> HttpMethod:
-        if self.input[self.position:self.position + len("GET") + 1].upper() == "GET":
+        if self.input[self.position:self.position + len("GET")].upper() == "GET":
+            self.position += len("GET")
             return HttpMethod.GET
-        elif self.input[self.position:self.position + len("POST") + 1].upper() == "POST":
+        elif self.input[self.position:self.position + len("POST")].upper() == "POST":
+            self.position += len("POST")
             return HttpMethod.POST
         raise ValueError(f"Expected a GET or POST method at position '{self.position}'.")
 
@@ -31,11 +33,8 @@ class InputParser:
                self.input[end_index] != ' ' and
                self.input[end_index] != '\r'):
             end_index += 1
-        # TODO: end_index + 1?
         parsed_string = self.input[self.position:end_index]
-        # The `+ 1` eats the space after the string.
-        # TODO: + 2 for \r\n?
-        self.position = end_index + 1
+        self.position = end_index
         return parsed_string
 
     def __try_parse_port(self) -> int:
@@ -47,9 +46,10 @@ class InputParser:
             # No digits were found. The port is optional, so we
             # don't throw an exception. Instead, we return None.
             return None
-        # end_index + 1 ??
+
+        parsed_port = int(self.input[self.position:end_index])
         self.position = end_index
-        return int(self.input[self.position:end_index])
+        return parsed_port
 
     def parse_input(self) -> list[HttpRequest]:
         state: ParsingState = ParsingState.EXPECTING_HTTP_METHOD
@@ -68,10 +68,10 @@ class InputParser:
             elif state == ParsingState.EXPECTING_FILE_NAME:
                 file_name = self.__parse_string()
                 self.__eat_required([" "], "A space is expected after file name.")
-                state == ParsingState.EXPECTING_HOST_NAME
+                state = ParsingState.EXPECTING_HOST_NAME
             elif state == ParsingState.EXPECTING_HOST_NAME:
                 host_name = self.__parse_string()
-                state == ParsingState.EXPECTING_OPTIONAL_PORT_NUMBER
+                state = ParsingState.EXPECTING_OPTIONAL_PORT_NUMBER
             elif state == ParsingState.EXPECTING_OPTIONAL_PORT_NUMBER:
                 if self.input[self.position] == " ":
                     # When parsing the host name, we don't eat the space.
@@ -82,13 +82,24 @@ class InputParser:
                 else:
                     # If there is no space, then the port number was omitted.
                     port_number = None
-                # Whether a port number was specified or not, a crlf must be found.
-                self.__eat_required(["\r\n"], f"Expected CRLF at position '{self.position}'.")
-                state == ParsingState.EXPECTING_OPTIONAL_HEADERS
+                # Whether a port number was specified or not, a crlf (or end of file) must be found.
+                if not self.__reached_end():
+                    self.__eat_required(["\r\n"], f"Expected CRLF at position '{self.position}'.")
+                state = ParsingState.EXPECTING_OPTIONAL_HEADERS
             elif state == ParsingState.EXPECTING_OPTIONAL_HEADERS:
                 # Not yet implemented.
                 requests.append(HttpRequest(method, file_name, host_name, port_number))
+                method = None
+                file_name = None
+                host_name = None
+                port_number = None
                 state = ParsingState.EXPECTING_HTTP_METHOD
+
+        # The whole file is now consumed.
+        if (state == ParsingState.EXPECTING_OPTIONAL_PORT_NUMBER or
+            state == ParsingState.EXPECTING_OPTIONAL_HEADERS): # TODO: Other optional states.
+            requests.append(HttpRequest(method, file_name, host_name, port_number))
+            state = ParsingState.EXPECTING_HTTP_METHOD
 
         if state != ParsingState.EXPECTING_HTTP_METHOD:
             raise ValueError(f"Reached end of file at state '{state.name}'.")
