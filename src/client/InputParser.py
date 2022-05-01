f6b9d761026aa05a1bbed7c00c33dbad75da42b4
Youssef1313 <youssefvictor00@gmail.com>
2022-05-01 11:09:10 +0200

    Refactor

--- src/client/InputParser.py
+++ src/client/InputParser.py
@@ -11,11 +11,10 @@ class InputParser:
     def __reached_end(self) -> bool:
         return self.position >= len(self.input)
 
-    def __eat_required(self, tokens_to_eat: list[str], error: str) -> None:
-        for token in tokens_to_eat:
-            if self.input[self.position:self.position + len(token)] == token:
-                self.position += len(token)
-                return
+    def __eat_required(self, token: str, error: str) -> None:
+        if self.input[self.position:self.position + len(token)] == token:
+            self.position += len(token)
+            return
         raise ValueError(error)
 
     def __parse_method(self) -> HttpMethod:
@@ -28,49 +27,56 @@ class InputParser:
         raise ValueError(f"Expected a GET or POST method at position '{self.position}'.")
 
     def __parse_string(self) -> str:
-        end_index = self.position
-        while (end_index < len(self.input) and
-               self.input[end_index] != ' ' and
-               self.input[end_index] != '\r'):
-            end_index += 1
-        parsed_string = self.input[self.position:end_index]
-        self.position = end_index
-        return parsed_string
+        start_index = self.position
+        while (not self.__reached_end() and
+               self.input[self.position] != ' ' and
+               self.input[self.position] != '\r'):
+            self.position += 1
+        return self.input[start_index:self.position]
 
     def __try_parse_port(self) -> int:
-        end_index = self.position
-        while (end_index < len(self.input) and
-               self.input[end_index].isdigit()):
-            end_index += 1
-        if end_index == self.position:
+        start_index = self.position
+        while (not self.__reached_end() and
+               self.input[self.position].isdigit()):
+            self.position += 1
+        if start_index == self.position:
             # No digits were found. The port is optional, so we
             # don't throw an exception. Instead, we return None.
             return None
 
-        parsed_port = int(self.input[self.position:end_index])
-        self.position = end_index
-        return parsed_port
+        return int(self.input[start_index:self.position])
 
     def parse_input(self) -> list[HttpRequest]:
+        class ParseData:
+            method: HttpMethod = None
+            host_name: str = None
+            file_name: str = None
+            port_number: int = None
+
+            def clear(self) -> None:
+                self.method = None
+                self.host_name = None
+                self.file_name = None
+                self.port_number = None
+
+            def to_request(self) -> HttpRequest:
+                return HttpRequest(self.method, self.file_name, self.host_name, self.port_number)
+
         state: ParsingState = ParsingState.EXPECTING_HTTP_METHOD
         requests: list[HttpRequest] = []
-
-        method: HttpMethod = None
-        host_name: str = None
-        file_name: str = None
-        port_number: int = None
+        data: ParseData = ParseData()
 
         while not self.__reached_end():
             if state == ParsingState.EXPECTING_HTTP_METHOD:
-                method = self.__parse_method()
-                self.__eat_required([" "], "A space is expected after Http method.")
+                data.method = self.__parse_method()
+                self.__eat_required(" ", "A space is expected after Http method.")
                 state = ParsingState.EXPECTING_FILE_NAME
             elif state == ParsingState.EXPECTING_FILE_NAME:
-                file_name = self.__parse_string()
-                self.__eat_required([" "], "A space is expected after file name.")
+                data.file_name = self.__parse_string()
+                self.__eat_required(" ", "A space is expected after file name.")
                 state = ParsingState.EXPECTING_HOST_NAME
             elif state == ParsingState.EXPECTING_HOST_NAME:
-                host_name = self.__parse_string()
+                data.host_name = self.__parse_string()
                 state = ParsingState.EXPECTING_OPTIONAL_PORT_NUMBER
             elif state == ParsingState.EXPECTING_OPTIONAL_PORT_NUMBER:
                 if self.input[self.position] == " ":
@@ -78,27 +84,24 @@ class InputParser:
                     # If there is a space, then we expect a port number.
                     # We increment position to eat the space and parse the port.
                     self.position += 1
-                    port_number = self.__try_parse_port()
+                    data.port_number = self.__try_parse_port()
                 else:
                     # If there is no space, then the port number was omitted.
-                    port_number = None
+                    data.port_number = None
                 # Whether a port number was specified or not, a crlf (or end of file) must be found.
                 if not self.__reached_end():
-                    self.__eat_required(["\r\n"], f"Expected CRLF at position '{self.position}'.")
+                    self.__eat_required("\r\n", f"Expected CRLF at position '{self.position}'.")
                 state = ParsingState.EXPECTING_OPTIONAL_HEADERS
             elif state == ParsingState.EXPECTING_OPTIONAL_HEADERS:
                 # Not yet implemented.
-                requests.append(HttpRequest(method, file_name, host_name, port_number))
-                method = None
-                file_name = None
-                host_name = None
-                port_number = None
+                requests.append(data.to_request())
+                data.clear()
                 state = ParsingState.EXPECTING_HTTP_METHOD
 
         # The whole file is now consumed.
         if (state == ParsingState.EXPECTING_OPTIONAL_PORT_NUMBER or
                 state == ParsingState.EXPECTING_OPTIONAL_HEADERS):  # TODO: Other optional states.
-            requests.append(HttpRequest(method, file_name, host_name, port_number))
+            requests.append(data.to_request())
             state = ParsingState.EXPECTING_HTTP_METHOD
 
         if state != ParsingState.EXPECTING_HTTP_METHOD:
