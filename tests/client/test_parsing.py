e5de535b0da5c3e15a0eca6b5cdd00ba26647300
Youssef1313 <youssefvictor00@gmail.com>
2022-05-09 11:19:10 +0200

    Fix tests

--- tests/client/test_parsing.py
+++ tests/client/test_parsing.py
@@ -6,13 +6,13 @@ from src.client.parse_input import parse_input
 def test_minimal_allowed():
     requests = parse_input("""GET file host""")
     assert len(requests) == 1
-    assert_request("file", "host", 80, HttpMethod.GET, requests[0])
+    assert_request("/file", "host", 80, HttpMethod.GET, requests[0])
 
 
 def test_minimal_allowed_with_port():
     requests = parse_input("""GET file host 8080""")
     assert len(requests) == 1
-    assert_request("file", "host", 8080, HttpMethod.GET, requests[0])
+    assert_request("/file", "host", 8080, HttpMethod.GET, requests[0])
 
 
 def assert_request(expected_filename: str,
