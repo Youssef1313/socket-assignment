f37b8c695dec8181a1cc2b2f42a3424513dae3be
Youssef1313 <youssefvictor00@gmail.com>
2022-05-09 11:11:38 +0200

    No need for a class

--- src/client/main.py
+++ src/client/main.py
@@ -3,14 +3,14 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 from src.client.WebClient import WebClient
-from src.client.InputParser import InputParser
+from src.client.parse_input import parse_input
 
 input_file = os.path.abspath(os.path.join(os.path.dirname(__file__), sys.argv[1]))
 with open(input_file, 'r') as f:
     commands = f.read()
 
 client = WebClient()
-requests = InputParser.parse_input(commands)
+requests = parse_input(commands)
 print(f"Input file contains {len(requests)} request(s).")
 
 for request in requests:
